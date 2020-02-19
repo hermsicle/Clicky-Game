fafd86730766ac066679cca528883277792d4b90
Herman Liu <hermanliu95@gmail.com>
2020-02-19 14:37:39 -0800

    first commit

---
+++ src/Components/Head/Head.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Head.css"
+
+function Head() {
+    return (
+        <div className="header">
+            <div className="container">
+                <h3>Clicky Game</h3>
+                <h3>Click an image to begin!</h3>
+                <h3>Score: 0</h3>
+                <h3>Top Score: 0</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Head
