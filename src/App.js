8fff47ec74e0480b619afebe50f9cc7c395af0a1
Herman Liu <hermanliu95@gmail.com>
2020-02-19 17:16:28 -0800

    added images

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import Head from "./Components/Head/Head";
 import Title from "./Components/Title/Title";
 import Main from "./Components/Main/Main";
 
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+
+
+
   render() {
     return (
       <div>
         <Head />
         <Title />
         <Main />
+
       </div>
     )
   }
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "./Main.css"
+import AcidRick from "../Assets/Images/acidrick.jpg"
+import AngryPlanet from "../Assets/Images/angryplanet.jpg"
+import AngrySun from "../Assets/Images/angrysun.jpg"
+import CoolRick from "../Assets/Images/coolrick.jpg"
+import DerpMorty from "../Assets/Images/derpmorty.jpg"
+import DerpRick from "../Assets/Images/derprick.jpg"
+import Doggy from "../Assets/Images/doggy.jpg"
+import EvilPickleRick from "../Assets/Images/evilpicklerick.jpg"
+import GiantDuck from "../Assets/Images/giantduck.jpg"
+import NurseRick from "../Assets/Images/nurserick.jpg"
+import PickleRick from "../Assets/Images/picklerick.jpg"
+import TerrifiedMorty from "../Assets/Images/terrifiedmorty.jpg"
 
-function Main() {
-    return (
-        <div className="main">
-            <div className="container3">
-
-            </div>
-        </div>
-    )
+class Main extends Component {
+    render() {
+        return (
+            <div className="app-container">
+                <div className="app">
+                    <img src={AcidRick} className="img" alt=""></img>
+                    <img src={AngryPlanet} className="img" alt=""></img>
+                    <img src={AngrySun} className="img" alt=""></img>
+                    <img src={CoolRick} className="img" alt=""></img>
+                    <img src={DerpMorty} className="img" alt=""></img>
+                    <img src={DerpRick} className="img" alt=""></img>
+                    <img src={Doggy} className="img" alt=""></img>
+                    <img src={EvilPickleRick} className="img" alt=""></img>
+                    <img src={GiantDuck} className="img" alt=""></img>
+                    <img src={NurseRick} className="img" alt=""></img>
+                    <img src={PickleRick} className="img" alt=""></img>
+                    <img src={TerrifiedMorty} className="img" alt=""></img>
+                </div>
+            </div >
+        )
+    }
 }
 
 export default Main
